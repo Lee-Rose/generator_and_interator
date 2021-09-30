e41b6cdfbb3f1b8715c81124b9351a6e4d2cd3cc
Lee <84514310+Lee-Rose@users.noreply.github.com>
2021-09-30 20:10:58 +0300

    function hash

--- get_hash.py
+++ get_hash.py
@@ -1,10 +1,23 @@
 import hashlib
 
-def compute_MD5_hash(string, encoding='utf-8'):
-    md5_hash = hashlib.md5()
-    md5_hash.update(string.encode(encoding))
-    return md5_hash.hexdigest()
+def get_hash(path,encoding='utf-8'):
 
-with open('dzen.txt') as f:
-    for line in f:
-        print(compute_MD5_hash(line))
+    '''a generator that takes a file path.
+    Returns the md5 hash of each line in the file on each iteration.'''
+
+    md5_hash = hashlib.md5()
+    with open(path) as f:
+        while True:
+            txt_line = f.readline()
+            if not txt_line:
+                break
+            md5_hash.update(txt_line.encode(encoding))
+            yield md5_hash.hexdigest()
+
+
+format_text = get_hash('dzen.txt')
+
+for item in format_text:
+    print(item)
+
+
